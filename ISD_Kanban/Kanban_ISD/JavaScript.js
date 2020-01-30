74d2c4fa95f13bcff14ee5a52d809a35b7121c8d
tamchrusist <35727899+tamchrusist@users.noreply.github.com>
2020-01-30 17:37:45 +0300

    Add files via upload

---
+++ ISD_Kanban/Kanban_ISD/JavaScript.js
@@ -0,0 +1,22 @@
+﻿var dragged;
+function drag(e) {
+    dragged = e.target;
+}
+function allowDrop(e) {
+    e.preventDefault();
+}
+function drop(e) {
+    e.preventDefault();
+
+    let parent;
+    if (e.currentTarget) {
+        parent = e.currentTarget
+    } else {
+        parent = e.path.filter((i) => {
+            if (i.classList) {
+                return i.classList.contains('kanban-list');
+            }
+        })[0];
+    }
+    parent.appendChild(dragged);
+}
