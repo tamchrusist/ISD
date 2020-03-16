1b56638b442bde937825c99e516bd1b7e9cea9ff
tamchrusist <35727899+tamchrusist@users.noreply.github.com>
2020-03-16 18:05:42 +0200

    Add files via upload

---
+++ ISD_Saver/Saver/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Saver
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {                
+            HttpWebRequest proxy_request = (HttpWebRequest)WebRequest.Create(WWW.Text);
+            proxy_request.Method = "GET";
+            proxy_request.ContentType = "application/x-www-form-urlencoded";
+            proxy_request.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.89 Safari/532.5";
+            proxy_request.KeepAlive = true;
+            HttpWebResponse resp = proxy_request.GetResponse() as HttpWebResponse;
+            string html = "";
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream(), Encoding.GetEncoding(1251)))
+            html = sr.ReadToEnd();
+            html = html.Trim();
+            Result.Text = html;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = @"C:\Users\User\source\repos\";
+            saveFileDialog.Filter = "Text file(*.txt)|*.txt|C# file(*.cs)|*.cs|XAML Files (*.xaml)|*.xaml|PDF files(*.pdf)|*.pdf";
+            if (saveFileDialog.ShowDialog() == true)
+            File.WriteAllText(saveFileDialog.FileName, Result.Text.ToString());
+        }
+    }
+}
